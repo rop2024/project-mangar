226574f55518d10c0a8fc49e58957a31becaa1e8
rop2024 <rohitpandey231456@gmail.com>
2025-09-09 09:06:10 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -4,24 +4,29 @@ import cors from "cors";
 const app = express();
 
 // basic configuration
-app.use(express.json({limit : "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 // cors configuration
-app.use(cors({
-    origin:process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
-    credentials : true,
-    methods : ["POST", "GET", "PATCH", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders : ["Content-Type", "Authorization"] 
-}))
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+		credentials: true,
+		methods: ["POST", "GET", "PATCH", "PUT", "DELETE", "OPTIONS"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	}),
+);
 
-app.get('/', (req, res) => {
-  res.send('This works lol!')
-})
+import healthCheckRoute from "./routes/healthcheck.controller.js";
+app.use("/api/v1/healthcheck", healthCheckRoute);
 
-app.get('/home', (req,res) => {
-  res.send("This is home page")
-})
+app.get("/", (req, res) => {
+	res.send("This works lol!");
+});
+
+app.get("/home", (req, res) => {
+	res.send("This is home page");
+});
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 import app from "./app.js";
-dotenv.config({ path: "./.env" });
+import connectDB from "./db/index.js";
 
+dotenv.config({ path: "./.env" });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+connectDB()
+	.then(
+		app.listen(port, () => {
+			console.log(`Example app listening on port ${port}`);
+		}),
+	)
+	.catch((err) => {
+		console.error("Database connection failed", err);
+		process.exit(1);
+	});
